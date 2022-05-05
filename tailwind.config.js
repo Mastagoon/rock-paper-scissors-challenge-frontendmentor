1f162880a63b5e2067a72fc54264469f775c21e6
= <inzilagiway@gmail.com>
2022-05-06 00:46:04 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                scissorsGradFrom: 'hsl(39, 89%, 49%)',
+                scissorsGradTo: 'hsl(40, 84%, 53%)',
+                paperGradFrom: 'hsl(230, 89%, 62%)',
+                papreGradTo: 'hsl(230, 89%, 65%)',
+                rockGradFrom: 'hsl(349, 71%, 52%)',
+                rockGradTo: 'hsl(349, 70%, 56%)',
+                cyanGradFrom: 'hsl(189, 59%, 53%)',
+                cyanGradTo: 'hsl(189, 58%, 57%)',
+                darkText: 'hsl(229, 25%, 31%)',
+                scoreText: 'hsl(229, 64%, 46%)',
+                headerOutline: 'hsl(217, 16%, 45%)',
+                bgFrom: 'hsl(214, 47%, 23%)',
+                bgTo: 'hsl(237, 49%, 15%)'
+            },
+        },
+    },
+    plugins: [],
+}
